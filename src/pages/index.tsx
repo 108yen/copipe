5d73a1e1aabcd94a0fa8dc19e5f07f7cf9e2ac7f
kazuki <kazuking.1911@gmail.com>
2023-02-12 18:20:08 +0900

    Grid入れた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import type { NextPage } from 'next'
-import { Container, Button, Box } from '@mui/material';
+import { Container, Button, Box, Grid, Card, CardContent } from '@mui/material';
 import SearchAppBar from './searchAppBar';
 
 const Home: NextPage = () => {
   return (
     <>
       <SearchAppBar />
-      <Container fixed>
+      <Box sx={{ flexGrow: 1, p: 3 }}>
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Card sx={{ bgcolor: 'primary.main' }}>
+              <CardContent>
+                xs=8
+              </CardContent>
+            </Card>
+            {/* xs=8 */}
+          </Grid>
+        </Grid>
+      </Box>
+      {/* <Container fixed>
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
           <Button variant="contained">Hello World</Button>
         </Box>
-      </Container>
+      </Container> */}
     </>
   )
 }
