bee85cfa1372fb5e8b847d99d0ce888bdb64a843
kazuki <kazuking.1911@gmail.com>
2023-02-18 00:17:52 +0900

    投稿ページのレイアウト変更

--- src/pages/postForm.tsx
+++ src/pages/postForm.tsx
@@ -1,32 +1,45 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from "@mui/material";
 import { NextPage } from "next";
 import SearchAppBar from "./modules/searchAppBar";
 
 const PostForm: NextPage = () => {
     return (
         <>
-            <SearchAppBar /><form>
-                <Typography variant="h6" gutterBottom>
-                    Create a new post
-                </Typography>
-                <TextField
-                    label="Title"
-                    fullWidth
-                    margin="normal" />
-                <TextField
-                    label="Content"
-                    fullWidth
-                    multiline
-                    rows={4}
-                    margin="normal" />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                >
-                    Submit
-                </Button>
-            </form>
+            <SearchAppBar />
+            <Box sx={{ flexGrow: 1, p: 3 }}>
+                <Grid container justifyContent="center" spacing={2}>
+                    <Grid item xs={12} md={7}>
+                        <Card>
+                            <CardContent>
+                                <form onSubmit={()=>{}}>
+                                    <TextField
+                                        label="Title"
+                                        fullWidth
+                                        margin="normal" />
+                                    <TextField
+                                        label="Content"
+                                        fullWidth
+                                        multiline
+                                        rows={4}
+                                        margin="normal" />
+                                    <Box sx={{
+                                        display: 'flex',
+                                        justifyContent:'center',
+                                    }}>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </Button>
+                                    </Box>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Box>
         </>
     );
 }
