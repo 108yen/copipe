84e9fabe04925eca6fffdf531a6f71d00a0ca055
kazuki <kazuking.1911@gmail.com>
2023-02-19 18:20:58 +0900

    サイトのアイコンとタイトル設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,9 @@ function MyApp(props: MyAppProps) {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>
+          copipe
+        </title>
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
