a0bb778c121a4fd9649364e90aaad66f738cb5f1
kazuki <kazuking.1911@gmail.com>
2023-02-21 22:33:16 +0900

    build error修正

--- src/pages/modules/copipeCard.tsx
+++ src/pages/modules/copipeCard.tsx
@@ -14,7 +14,7 @@ const CopipeCard: React.FC<Props> = ({ copipeList }) => {
             sx={{ m: theme.spacing(2) }}
         >
             <CardContent>
-                {Object.values(copipeList).map((e) => CopipeItemWidget(e))}
+                {copipeList.map((e) => CopipeItemWidget(e))}
             </CardContent>
         </Card>
     );
