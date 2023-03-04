c5b48c22665236c3d36eebfba5bf7aa5751aecd1
kazuki <kazuking.1911@gmail.com>
2023-03-04 16:31:35 +0900

    next sitemapのほうにrobots.txtの設定追加

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     siteUrl: 'https://www.netcopipe.com',
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: '*', allow: '/' },
+            { userAgent: '*', disallow: '/postForm' }
+        ],
+    },
 };
