f1ba5a7177180f035b69fef7d792c4a907da4612
kazuki <kazuking.1911@gmail.com>
2023-03-18 18:20:35 +0900

    jotaiのリファクタリング
    datafetchの実装をatomに格納

--- src/modules/searchForm.tsx
+++ src/modules/searchForm.tsx
@@ -3,9 +3,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import { Button, FormControl, InputAdornment, TextField, Card, Box, IconButton } from '@mui/material';
 import theme from '@/theme';
 import { useAtom } from 'jotai';
-import { Copipe, searchTextAtom, textFormAtom } from '../components/Atoms';
+import { searchTextAtom, textFormAtom } from '../components/Atoms';
 import styled from '@emotion/styled';
-import supabase from '@/utils/supabase';
 import router from 'next/router';
 
 const SearchCard = styled(Card)(() => ({
@@ -14,22 +13,16 @@ const SearchCard = styled(Card)(() => ({
     zIndex: 100,
 }));
 
-type Props = {
-    setSearchText: (text: string) => void;
-}
-
-const SearchForm: React.FC<Props> = ({
-    setSearchText,
-}) => {
+const SearchForm: React.FC = () => {
     const [text, setText] = useAtom(textFormAtom);
+    const [, setSearchText] = useAtom(searchTextAtom);
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         if (event.key == 'Enter') {
             handleSubmit();
         }
     }
-    const handleSubmit = async () => {
-        //todo:検索処理
+    const handleSubmit = () => {
         setSearchText(text);
         window.scrollTo({
             top: 0,
@@ -39,24 +32,6 @@ const SearchForm: React.FC<Props> = ({
     const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { value } = event.target;
         setText(value);
-    }
-    const searchCopipe = async (word: string) => {
-        const { data, error, count } = await supabase
-            .from('copipe')
-            .select('*', { count: 'exact' })
-            .like('body', '%' + word + '%');
-        const copipes: Array<Copipe> = data != null ? data.map(e => {
-            const copipeItem: Copipe = {
-                id: e.id,
-                inserted_at: e.inserted_at,
-                updated_at: e.updated_at,
-                body: e.body,
-                title: e.title,
-            };
-            return copipeItem;
-        }) : [];
-
-        return copipes;
     }
 
     return (
