1a7c96f6349079ca9d16b8e887f5ba3c43f207df
kazuki <kazuking.1911@gmail.com>
2024-01-08 01:40:34 +0900

    revalidate追記

--- src/app/archives/[id]/serverActions.tsx
+++ src/app/archives/[id]/serverActions.tsx
@@ -4,16 +4,18 @@ import supabase from "@/utils/supabase";
 import { revalidatePath } from "next/cache";
 
 export async function postComment(copipe_id: number, body: string) {
-    const { data, error } = await supabase
-        .from('comments')
-        .insert([
-            {
-                body: body,
-                copipe_id: copipe_id
-            },
-        ]);
-    if (error) {
-        revalidatePath('/archives/[id]','page')
+    try {        
+        await supabase
+            .from('comments')
+            .insert([
+                {
+                    body: body,
+                    copipe_id: copipe_id
+                },
+            ]);
+    } catch (error) {
         return { error: JSON.stringify(error) }
-    };
+    } finally {
+        revalidatePath('/archives/[id]', 'page')
+    }
 }
