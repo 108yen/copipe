75f38f92bac8ca350b52e581376ed5776b8e9d6e
kazuki <kazuking.1911@gmail.com>
2024-01-08 17:07:58 +0900

    GA追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,58 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { Metadata } from 'next'
 import ThemeRegistry from "@/theme/themeRegistry";
 import SearchAppBar from "@/modules/searchAppBar";
 import Grid from "@mui/material/Grid";
 import AdmaxPCSideVertical from "@/ad/admax/pcSideVertical";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://www.netcopipe.com/"),
     title: {
         default: 'copipe | コピペのアーカイブ',
         template: '%s | copipe'
     },
-    description: "2ch/5chやまとめサイトなどで話題になった有名なコピペや、笑えるコピペを収集しています。",
+    description: "2ch/5chやまとめサイトなどで話題になった有名なコピペや、笑えるコピペを収集しています。コピペの検索、投稿、コメントが可能です。",
     openGraph: {
+        type: `website`,
         url: "https://www.netcopipe.com/",
-        title: "copipe | 2chコピペネタ帳",
-        description: "コピペが検索できるサイト。",
+        title: "copipe | コピペのアーカイブ",
+        description: "2ch/5chやまとめサイトなどで話題になった有名なコピペや、笑えるコピペを収集しています。コピペの検索、投稿、コメントが可能です。",
         images: [
             {
                 url: "https://www.netcopipe.com/android-chrome-512x512.png",
+                type: `image/png`
             },
         ],
     },
     twitter: {
-        site: '@site',
-        images: [
-            {
-                url: "https://www.netcopipe.com/android-chrome-512x512.png",
-            },
-        ],
+        card: `summary`
     }
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return <html lang="ja">
-        <body>
-            <ThemeRegistry options={{ key: `css`, prepend: true }}>
-                <SearchAppBar />
-                <Grid container justifyContent="center" spacing={1} marginY={1}>
-                    <Grid item xs={12} md={9} lg={8} xl={6}>
-                        {children}
+    return (
+        <html lang="ja">
+            <head>
+                <Suspense>
+                    {process.env.NODE_ENV === "production" && (
+                        <GoogleAnalytics gaId='G-RT0VSWZG3X' />
+                    )}
+                </Suspense>
+            </head>
+            <body>
+                <ThemeRegistry options={{ key: `css`, prepend: true }}>
+                    <SearchAppBar />
+                    <Grid container justifyContent="center" spacing={1} marginY={1}>
+                        <Grid item xs={12} md={9} lg={8} xl={6}>
+                            {children}
+                        </Grid>
+                        <Grid item md={1} display={{ md: 'flex', xs: 'none' }}>
+                            <AdmaxPCSideVertical />
+                        </Grid>
                     </Grid>
-                    <Grid item md={1} display={{ md: 'flex', xs: 'none' }}>
-                        <AdmaxPCSideVertical />
-                    </Grid>
-                </Grid>
-            </ThemeRegistry>
-        </body>
-    </html>
+                </ThemeRegistry>
+            </body>
+        </html>
+    )
 }
