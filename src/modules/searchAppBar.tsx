ba0b4da2c353ffc904317890b9597e0bbeccfb97
kazuki <kazuking.1911@gmail.com>
2024-01-11 18:17:35 +0900

    headerにaboutページのリンク追加

--- src/modules/searchAppBar.tsx
+++ src/modules/searchAppBar.tsx
@@ -5,33 +5,61 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 
 export default function SearchAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" elevation={0} sx={{ backgroundColor: 'primary.light' }}>
                 <Toolbar>
-                    <Link
-                        href='/'
-                        style={{
-                            textDecoration: 'none',
-                            overflow: 'hidden'
-                        }}
+                    <Stack
+                        direction='row'
+                        flexGrow={1}
+                        alignItems='center'
+                        justifyContent='space-between'
                     >
-                        <Typography
-                            variant="h5"
-                            fontWeight='bold'
-                            noWrap
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'block' },
-                                color: 'text.primary',
+                        <Link
+                            href='/'
+                            style={{
                                 textDecoration: 'none',
+                                overflow: 'hidden'
                             }}
                         >
-                            copipe
-                        </Typography>
-                    </Link>
+                            <Typography
+                                variant="h5"
+                                fontWeight='bold'
+                                noWrap
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'block' },
+                                    color: 'text.primary',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                copipe
+                            </Typography>
+                        </Link>
+                        <Link
+                            href='/about'
+                            style={{
+                                textDecoration: 'none',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <Typography
+                                variant="caption"
+                                noWrap
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'block' },
+                                    color: 'text.disabled',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                このサイトについて
+                            </Typography>
+                        </Link>
+                    </Stack>
                 </Toolbar>
             </AppBar>
             <Divider />
