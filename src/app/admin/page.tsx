7b630b4bd27140241a14fe7f864db4d7d7db19e3
kazuki <kazuking.1911@gmail.com>
2024-01-12 15:38:38 +0900

    文字削減実装

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,10 +15,41 @@ export default async function page() {
         body: data[0].body,
         tags: data[0].tags
     }
+    const url = `https://www.netcopipe.com/archives/${copipe.copipe_id}`
+    const text = encodeURIComponent(reductionText(copipe.body, url))
 
-    const twitterLink = `https://twitter.com/intent/tweet?url=https://www.netcopipe.com/archives/${copipe.copipe_id}&text=${copipe.body
+    const twitterLink = `https://twitter.com/intent/tweet?url=${url}&text=${text
         }`;
-    
+
+    function reductionText(text: string, url: string) {
+        let reductionCount = 0
+        while (!checkTweetLength(`${text.slice(0, text.length - reductionCount)}...${url}`)) {
+            reductionCount++
+        }
+        return `${text.slice(0, text.length - reductionCount)}...`
+    }
+
+    function countCharacters(text: string): { fullWidth: number; halfWidth: number } {
+        let fullWidth = 0;
+        let halfWidth = 0;
+        for (const char of text) {
+            // 全角文字かどうかを判定
+            if (char.match(/[^\x01-\x7E\xA1-\xDF]/)) {
+                fullWidth += 2;
+            } else {
+                halfWidth += 1;
+            }
+        }
+
+        return { fullWidth, halfWidth };
+    }
+
+    function checkTweetLength(text: string): boolean {
+        const { fullWidth, halfWidth } = countCharacters(text);
+        const totalCharacters = fullWidth + halfWidth;
+        return fullWidth <= 140 && totalCharacters <= 280
+    }
+
     return (
         <>
             <Stack direction='row' spacing={2}>
