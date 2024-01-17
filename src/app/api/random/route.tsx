7daea33ca5e52585b5c2db7bdc879a491bb94901
kazuki <kazuking.1911@gmail.com>
2024-01-17 22:48:56 +0900

    random apiにforce-dynamic追加

--- src/app/api/random/route.tsx
+++ src/app/api/random/route.tsx
@@ -1,6 +1,8 @@
 import getTweet from '@/utils/tweet';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const result = await getTweet()
     return NextResponse.json(result);
