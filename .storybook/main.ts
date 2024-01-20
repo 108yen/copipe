5f956760ea93bbc873320cd5628023dc3a15a38a
108yen <kazuking.1911@gmail.com>
2024-01-21 01:30:57 +0900

    storybookでdarkmode切り替え実装

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-backgrounds",
+    'storybook-dark-mode'
   ],
   framework: {
     name: "@storybook/nextjs",
