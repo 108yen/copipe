f367263878f843d3451a8024ee33ed455f0503ce
108yen <kazuking.1911@gmail.com>
2024-01-22 00:50:10 +0900

    adminのLayout.tsx削除
    layout側でsearchParams取れないので

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,15 +2,24 @@ import { CopipeWithTag } from "@/models/copipeWithTag";
 import supabase from "@/utils/supabase"
 import { Tag } from "@/models/tag"
 import AdminPageTemplate from "@/modules/admin";
+import CopipePagination from "@/modules/copipePagination";
+import { notFound } from "next/navigation";
 
-export default async function Page({ params }: { params: { page: string } }) {
-    const page = Number(params.page)
-    const fetchCopipeWithTag = await supabase
+export const revalidate = 3600
+
+export default async function Page({ searchParams }: {
+    searchParams: { [key: string]: string | string[] | undefined }
+}) {
+    const page = typeof searchParams.page === 'string'
+        ? Number(searchParams.page) : 1
+    const { data, error, status, count } = await supabase
         .from('copipe_with_tag')
-        .select('*')
+        .select('*', { count: 'exact' })
         .order('copipe_id', { ascending: false })
         .range((page - 1) * 100, 100 * page - 1);
-    const copipes: CopipeWithTag[] = fetchCopipeWithTag.data!.map(value => {
+
+    if (error) notFound()
+    const copipes: CopipeWithTag[] = data!.map(value => {
         return {
             copipe_id: value.copipe_id,
             body: value.body,
@@ -29,5 +38,13 @@ export default async function Page({ params }: { params: { page: string } }) {
             body: value.body
         }
     })
-    return <AdminPageTemplate copipes={copipes} tags={tags} />
+    return (
+        <>
+            <AdminPageTemplate copipes={copipes} tags={tags} />
+            <CopipePagination
+                url="/admin"
+                total={count ? Math.ceil(count / 100) : 0}
+                page={page} />
+        </>
+    )
 }
