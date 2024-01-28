06d1530f2caffedcdb71ad52c2302e201dcedfd3
108yen <kazuking.1911@gmail.com>
2024-01-28 19:56:08 +0900

    横のカードをprisma移行

--- src/modules/tagListCard/index.tsx
+++ src/modules/tagListCard/index.tsx
@@ -1,24 +1,21 @@
 import { Tag } from "@/models/tag"
-import supabase from "@/utils/supabase"
 import { cache } from "react"
-import LoadingTagListCard from "./loading"
 import TagListCardTemplate from "./tagListCard"
+import { prisma } from "@/db/db"
 
 const fetchTags = cache(async () => {
+    const tags = await prisma.tag.findMany()
     console.log('tag get')
-    return await supabase
-        .from('tag')
-        .select()
+    return tags
 })
 
 export default async function TagListCard() {
-    const { data, error } = await fetchTags()
-    if (data == null) return <LoadingTagListCard />
+    const data = await fetchTags()
 
     const tags: Tag[] = data.map(
         tag => {
             return {
-                id: tag.id,
+                id: Number(tag.id),
                 created_at: new Date(tag.created_at),
                 body: tag.body
             }
