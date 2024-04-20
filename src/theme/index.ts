e996b571b545863fc60950fc5bbccaaa91bdd509
108yen <kazuking.1911@gmail.com>
2024-04-20 18:49:35 +0900

    feat(theme): added color token

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,17 +1,19 @@
-"use client";
-import { extendConfig, extendTheme, UsageTheme } from "@yamada-ui/react";
-import { semantics } from "./semantics";
-import styles from "./styles";
-import { customConfig } from "./config";
-import components from "./components";
+"use client"
+import { extendConfig, extendTheme, UsageTheme } from "@yamada-ui/react"
+import { semantics } from "./semantics"
+import styles from "./styles"
+import { customConfig } from "./config"
+import components from "./components"
+import tokens from "./tokens"
 
 const customTheme: UsageTheme = {
     styles,
     components,
     semantics,
-};
+    ...tokens,
+}
 
-export const theme = extendTheme(customTheme)();
-export const config = extendConfig(customConfig);
+export const theme = extendTheme(customTheme)()
+export const config = extendConfig(customConfig)
 
-export default theme;
+export default theme
---
+++ src/theme/tokens/colors.ts
@@ -0,0 +1,17 @@
+import { ThemeTokens } from "@yamada-ui/react"
+
+export const colors: ThemeTokens = {
+  whaleShark: {
+    "50": "#e1e7ea",
+    "100": "#d2dce0",
+    "200": "#b5c4cc",
+    "300": "#97adb7",
+    "400": "#7a95a3",
+    "500": "#607c8a",
+    "600": "#4f6672",
+    "700": "#3e5059",
+    "800": "#2d3a40",
+    "900": "#1c2428",
+    "950": "#13191b",
+  },
+}
