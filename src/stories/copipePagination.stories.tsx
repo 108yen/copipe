8f35724b0089041c71bbb929a85d8cd1c303bb94
108yen <kazuking.1911@gmail.com>
2024-04-26 23:14:57 +0900

    fix(stories): fix lint error

--- src/stories/copipePagination.stories.tsx
+++ src/stories/copipePagination.stories.tsx
@@ -1,38 +1,37 @@
 import { Meta, StoryObj } from "@storybook/react"
-import { userEvent, waitFor, within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
-import CopipePagination from "@/modules/copipePagination";
+import { userEvent, within } from "@storybook/testing-library"
+import CopipePagination from "@/modules/copipePagination"
 
 const meta = {
-    title: 'yamadaui/CopipePagination',
-    component: CopipePagination,
-    tags: ['autodocs']
+  title: "yamadaui/CopipePagination",
+  component: CopipePagination,
+  tags: ["autodocs"],
 } satisfies Meta<typeof CopipePagination>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {
-        url: '/test',
-        total: 20,
-        page: 1,
-    },
-    play: async ({ canvasElement }) => {
-        const canvas = within(canvasElement)
-        const page = canvas.getByText('2')
-        await userEvent.click(page)
-    }
+  args: {
+    url: "/test",
+    total: 20,
+    page: 1,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const page = canvas.getByText("2")
+    await userEvent.click(page)
+  },
 }
 export const Middle: Story = {
-    args: {
-        url: '/test',
-        total: 20,
-        page: 10,
-    },
-    play: async ({ canvasElement }) => {
-        const canvas = within(canvasElement)
-        const page = canvas.getByText('9')
-        await userEvent.click(page)
-    }
+  args: {
+    url: "/test",
+    total: 20,
+    page: 10,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const page = canvas.getByText("9")
+    await userEvent.click(page)
+  },
 }
