ad13154384e0b280f40ebe5ba8b01b99ff36f22e
108yen <kazuking.1911@gmail.com>
2024-11-13 21:54:34 +0900

    chore: change archives page to ppr

--- src/app/archives/[id]/page.tsx
+++ src/app/archives/[id]/page.tsx
@@ -1,24 +1,26 @@
-import { fetchCopipe } from "@/db/server/copipes"
-import Comment from "@/modules/comment"
-import { CopipeCardItem } from "@/modules/copipeCardItem"
+import { CopipeCardItemSkelton } from "@/modules/copipeCardItemSkeleton"
 import { Container, VStack } from "@yamada-ui/react"
-import ArchivesPagination from "./archivesPagination"
-import { checkBeforeAndAfterPage } from "./utils"
+import { Suspense } from "react"
+import {
+  PPRArchivesPagination,
+  PPRComment,
+  PPRCopipeCardItem,
+} from "./ppr-wrap"
 
-export const revalidate = 3600
+export const experimental_ppr = true
 
 export default async function page({ params }: { params: { id: string } }) {
-  const id = Number(params.id)
-  const copipe = await fetchCopipe(id)
-  const { beforeId, afterId } = await checkBeforeAndAfterPage(id)
-
   return (
     <VStack>
       <Container>
-        <CopipeCardItem copipeItem={copipe} />
+        <Suspense fallback={<CopipeCardItemSkelton />}>
+          <PPRCopipeCardItem params={params} />
+        </Suspense>
       </Container>
-      <Comment comments={copipe.comments} copipe_id={id} />
-      <ArchivesPagination beforeId={beforeId} afterId={afterId} />
+      <Suspense>
+        <PPRComment params={params} />
+      </Suspense>
+      <PPRArchivesPagination params={params} />
     </VStack>
   )
 }
---
+++ src/app/archives/[id]/ppr-wrap.tsx
@@ -0,0 +1,30 @@
+import { fetchCopipe } from "@/db/server/copipes"
+import Comment from "@/modules/comment"
+import { CopipeCardItem } from "@/modules/copipeCardItem"
+import ArchivesPagination from "./archivesPagination"
+import { checkBeforeAndAfterPage } from "./utils"
+
+interface Props {
+  params: { id: string }
+}
+
+export async function PPRCopipeCardItem({ params }: Props) {
+  const id = Number(params.id)
+  const copipe = await fetchCopipe(id)
+
+  return <CopipeCardItem copipeItem={copipe} />
+}
+
+export async function PPRComment({ params }: Props) {
+  const id = Number(params.id)
+  const { comments } = await fetchCopipe(id)
+
+  return <Comment comments={comments} copipe_id={id} />
+}
+
+export async function PPRArchivesPagination({ params }: Props) {
+  const id = Number(params.id)
+  const { beforeId, afterId } = await checkBeforeAndAfterPage(id)
+
+  return <ArchivesPagination beforeId={beforeId} afterId={afterId} />
+}
