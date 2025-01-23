61cb813cd75cc8510b49863053290fe976cd98b3
108yen <kazuking.1911@gmail.com>
2025-01-23 16:51:01 +0900

    feat: added `loading.tsx`

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Center, Loading } from "@yamada-ui/react"
+
+export default function loading() {
+  return (
+    <Center h="3xs" w="full">
+      <Loading fontSize="2xl" />
+    </Center>
+  )
+}
