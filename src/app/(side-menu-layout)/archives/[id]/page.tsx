c161645ead8fcbbd3a4071c5dc15b476b0c5b0d7
108yen <kazuking.1911@gmail.com>
2025-01-27 10:58:01 +0900

    fix: removed `dynamic` and added `generateStaticParams`

--- src/app/(side-menu-layout)/archives/[id]/page.tsx
+++ src/app/(side-menu-layout)/archives/[id]/page.tsx
@@ -3,7 +3,9 @@ import { Suspense } from "react"
 import ArchivePageLoading from "./page-loading"
 import { ArchivePageTemplate } from "./page-template"
 
-export const dynamic = "force-static"
+export async function generateStaticParams() {
+  return []
+}
 
 export default function page({ params }: { params: Promise<{ id: string }> }) {
   return (
