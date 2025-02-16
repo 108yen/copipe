7b8d006506311168095cd4b74b598519f4c663b9
108yen <kazuking.1911@gmail.com>
2025-02-16 22:26:20 +0900

    chore: added `loading.tsx` to `(side-menu-layout)` folder

--- src/app/(side-menu-layout)/archives/[id]/page.tsx
+++ src/app/(side-menu-layout)/archives/[id]/page.tsx
@@ -1,10 +1,15 @@
 import SideMenuLayout from "@/modules/layouts/sideMenuLayout"
+import { Metadata } from "next/types"
 import { Suspense } from "react"
 
 import ArchivePageLoading from "./page-loading"
 import { ArchivePageTemplate } from "./page-template"
 
-export default function page({ params }: { params: Promise<{ id: string }> }) {
+export const metadata: Metadata = {
+  title: "アーカイブ",
+}
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
   return (
     <SideMenuLayout>
       <Suspense fallback={<ArchivePageLoading />}>
