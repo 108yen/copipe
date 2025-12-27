8a03a53383941e3fc5d5788b27077d8234db99e4
108yen <kazuking.1911@gmail.com>
2025-12-27 21:50:34 +0900

    chore: added header option

---
+++ .changeset/six-tigers-write.md
@@ -0,0 +1,5 @@
+---
+"copipe": patch
+---
+
+Added `Cache-Control` header option for use Vercel CDN.
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,43 @@ import { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=3600, max-age=60",
+          },
+          {
+            key: "CDN-Cache-Control",
+            value: "max-age=3600",
+          },
+          {
+            key: "Vercel-CDN-Cache-Control",
+            value: "max-age=3600",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Document-Policy",
+            value: "js-profiling",
+          },
+        ],
+        source: "/(.*)",
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
