a9c37dfe53dc99f2c63324c15756db6195cf5526
108yen <kazuking.1911@gmail.com>
2026-01-02 21:50:30 -0500

    feat: migrate tag page

--- src/app/(side-menu-layout)/tag/[tagId]/page.tsx
+++ src/app/(side-menu-layout)/tag/[tagId]/page.tsx
@@ -1,9 +1,7 @@
+import { Center, Loading } from "@yamada-ui/react"
 import { NextPageProps } from "next"
-import { notFound } from "next/navigation"
 import { Metadata } from "next/types"
-import { fetchTagCopipes } from "@/db/server/copipes"
-import { fetchTag } from "@/db/server/tags"
-import { tagPageScheme } from "@/schemes"
+import { Suspense } from "react"
 import { TagPageTemplate } from "@/ui/templates"
 
 export const metadata: Metadata = {
@@ -14,26 +12,15 @@ export default async function Page({
   params,
   searchParams,
 }: NextPageProps<{ tagId: string }>) {
-  const { tagId } = await params
-  const { page } = await searchParams
-
-  const parseResult = tagPageScheme.safeParse({ page, tagId })
-
-  if (!parseResult.success) {
-    notFound()
-  }
-
-  const tagName = await fetchTag(parseResult.data.tagId)
-  const data = await fetchTagCopipes(
-    parseResult.data.tagId,
-    parseResult.data.page,
-  )
-
   return (
-    <>
-      <title>{`${tagName} | copipe`}</title>
-
-      <TagPageTemplate data={data} {...parseResult.data} />
-    </>
+    <Suspense
+      fallback={
+        <Center h="3xs" w="full">
+          <Loading fontSize="2xl" />
+        </Center>
+      }
+    >
+      <TagPageTemplate params={params} searchParams={searchParams} />
+    </Suspense>
   )
 }
--- src/db/server/tags.ts
+++ src/db/server/tags.ts
@@ -1,42 +1,42 @@
-import { unstable_cache } from "next/cache"
+import { cacheTag } from "next/cache"
 import { notFound } from "next/navigation"
 import { Tag } from "@/models/tag"
 import { prisma } from "../db"
 
-export const fetchTags = unstable_cache(
-  async function () {
-    const tags = await prisma.tag.findMany()
+export async function fetchTags() {
+  "use cache: remote"
+  cacheTag("tags")
 
-    console.log("get tag list")
+  const tags = await prisma.tag.findMany()
 
-    const result: Tag[] = tags.map((tag) => {
-      return {
-        body: tag.body,
-        created_at: new Date(tag.created_at),
-        id: tag.id,
-      }
-    })
+  console.log("get tag list")
 
-    return result
-  },
-  ["tags"],
-)
+  const result: Tag[] = tags.map((tag) => {
+    return {
+      body: tag.body,
+      created_at: new Date(tag.created_at),
+      id: tag.id,
+    }
+  })
 
-export type FetchTagsReturn = ReturnType<typeof fetchTags>
-
-export const fetchTag = unstable_cache(
-  async function (id: number) {
-    const tag = await prisma.tag
-      .findUniqueOrThrow({
-        select: { body: true },
-        where: { id },
-      })
-      .catch(() => {
-        notFound()
-      })
-    return tag
-  },
-  ["tag"],
-)
+  return result
+}
 
 export type FetchTagReturn = ReturnType<typeof fetchTag>
+
+export async function fetchTag(id: number) {
+  "use cache: remote"
+  cacheTag("tag")
+
+  const tag = await prisma.tag
+    .findUniqueOrThrow({
+      select: { body: true },
+      where: { id },
+    })
+    .catch(() => {
+      notFound()
+    })
+  return tag
+}
+
+export type FetchTagsReturn = ReturnType<typeof fetchTags>
